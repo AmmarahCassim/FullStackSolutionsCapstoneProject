36b55b6d093fc9323e264f649d8c1f953c725375
davebehr1 <davebehr1@gmail.com>
2018-03-23 09:09:38 +0200

    upload and download sound

--- Server.js
+++ Server.js
@@ -1,30 +1,155 @@
-var express	=	require("express");
-var MongoClient = require('mongodb').MongoClient
-var multer	=	require('multer');
-var path = require('path');
-var app	=	express();
-app.use(express.static(path.join(__dirname, '/public')));
-var storage	=	multer.diskStorage({
-  destination: function (req, file, callback) {
-    callback(null, './uploads');
-  },
-  filename: function (req, file, callback) {
-    callback(null, file.fieldname + '-' + Date.now());
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const crypto = require('crypto');
+const mongoose = require('mongoose');
+const multer = require('multer');
+const GridFsStorage = require('multer-gridfs-storage');
+const Grid = require('gridfs-stream');
+const methodOverride = require('method-override');
+//const cons = require('consolidate');
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+app.use(methodOverride('_method'));
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+
+// Mongo URI
+const mongoURI = 'mongodb://localhost/myDB';
+
+// Create mongo connection
+const conn = mongoose.createConnection(mongoURI);
+
+// Init gfs
+let gfs;
+
+conn.once('open', () => {
+  // Init stream
+  gfs = Grid(conn.db, mongoose.mongo);
+  gfs.collection('uploads');
+});
+
+// Create storage engine
+const storage = new GridFsStorage({
+  url: mongoURI,
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      crypto.randomBytes(16, (err, buf) => {
+        if (err) {
+          return reject(err);
+        }
+        const filename = buf.toString('hex') + path.extname(file.originalname);
+        const fileInfo = {
+          filename: filename,
+          bucketName: 'uploads'
+        };
+        resolve(fileInfo);
+      });
+    });
   }
 });
 var upload = multer({ storage : storage}).single('userPhoto');
 
-app.get('/',function(req,res){
-      res.sendFile(__dirname + "/index.html");
+// @route GET /
+// @desc Loads form
+app.get('/', (req, res) => {
+  gfs.files.find().toArray((err, files) => {
+    // Check if files
+    if (!files || files.length === 0) {
+      res.render('index', { files: false });
+    } else {
+      files.map(file => {
+        if (
+          file.contentType === 'audio/wav' ||
+          file.contentType === 'audio/mp3'
+        ){
+          file.isAudio = true;
+        } else {
+          file.isAudio = false;
+        }
+      });
+     
+      //alert(soundfile);
+      res.render('index', { files: files});
+    }
+  });
 });
 
-app.post('/api/photo',function(req,res){
-	upload(req,res,function(err) {
-		if(err) {
-			return res.end("Error uploading file.");
-		}
-		res.end("File is uploaded");
-	});
+// @route POST /upload
+// @desc  Uploads file to DB
+app.post('/upload', function(req, res){
+  upload(req,res,function(err) {
+    if(err) {
+      return res.end("Error uploading file.");
+    }
+    //res.json({ file: req.file });
+    res.redirect('/');
+});
+
+ 
+});
+
+app.get('/files', (req, res) => {
+  gfs.files.find().toArray((err, files) => {
+    // Check if files
+    if (!files || files.length === 0) {
+      return res.status(404).json({
+        err: 'No files exist'
+      });
+    }
+
+    // Files exist
+    return res.json(files);
+  });
+});
+
+app.get('/files/:filename', (req, res) => {
+  gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+    // Check if file
+    if (!file || file.length === 0) {
+      return res.status(404).json({
+        err: 'No file exists'
+      });
+    }
+    // File exists
+    return res.json(file);
+  });
+});
+
+app.get('/audio/:filename', (req, res) => {
+  gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+    // Check if file
+    if (!file || file.length === 0) {
+      return res.status(404).json({
+        err: 'No file exists'
+      });
+    }
+
+    // Check if image
+    if (file.contentType === 'audio/wav' || file.contentType === 'audio/mpeg') {
+      // Read output to browser
+      const readstream = gfs.createReadStream(file.filename);
+      readstream.pipe(res);
+    } else {
+      res.status(404).json({
+        err: 'Not an image'
+      });
+    }
+  });
+});
+
+app.delete('/files/:id', (req, res) => {
+  gfs.remove({ _id: req.params.id, root: 'uploads' }, (err, gridStore) => {
+    if (err) {
+      return res.status(404).json({ err: err });
+    }
+
+    res.redirect('/');
+    conn.dropDatabase();
+  });
 });
 
 app.listen(3000,function(){
