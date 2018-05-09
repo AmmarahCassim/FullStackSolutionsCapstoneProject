0bfaa4352f2a756ca3b89a9f9d042547ace07ce8
AmmarahCassim <34282478+AmmarahCassim@users.noreply.github.com>
2018-05-09 13:26:16 +0200

    Update test1 permissions

--- testfiles/test1.js
+++ testfiles/test1.js
@@ -1,12 +1,12 @@
  beforeEach(function() {
     if (process.env.SAUCE_USERNAME != undefined) {
       this.browser = new webdriver.Builder()
-      .usingServer('http://'+ process.env.AmmarahCassim+':'+process.env.24302255-f2d7-46f5-ac1c-9c90ec0eff11+'@ondemand.saucelabs.com:80/wd/hub')
+      .usingServer('http://'+ process.env."AmmarahCassim"+':'+process.env."24302255-f2d7-46f5-ac1c-9c90ec0eff11"+'@ondemand.saucelabs.com:80/wd/hub')
       .withCapabilities({
         'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
         build: process.env.TRAVIS_BUILD_NUMBER,
-        username: process.env.AmmarahCassim,
-        accessKey: process.env.24302255-f2d7-46f5-ac1c-9c90ec0eff11,
+        username: process.env."AmmarahCassim",
+        accessKey: process.env."24302255-f2d7-46f5-ac1c-9c90ec0eff11",
         browserName: "chrome"
       }).build();
     } else {
@@ -16,5 +16,5 @@
       }).build();
     }
 
-    return this.browser.get("http://localhost:3000/page/index.html");
+    return this.browser.get("http://localhost:3000/views/index.ejs");
 });
