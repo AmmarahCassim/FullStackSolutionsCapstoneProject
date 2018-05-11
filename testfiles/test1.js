be488a427d12b785a412b5ce6fed37dd1b0b80e8
AmmarahCassim <34282478+AmmarahCassim@users.noreply.github.com>
2018-05-11 09:53:26 +0200

    Update test1.js

--- testfiles/test1.js
+++ testfiles/test1.js
@@ -1,11 +1,15 @@
- beforeEach(function() {
+var assert = require("assert");
+var webdriver = require("selenium-webdriver");
+
+describe("testing javascript in the browser", function() {
+  beforeEach(function() {
     if (process.env."AmmarahCassim" != undefined) {
       this.browser = new webdriver.Builder()
       .usingServer('http://'+ process.env."AmmarahCassim"+':'+process.env."24302255-f2d7-46f5-ac1c-9c90ec0eff11"+'@ondemand.saucelabs.com:80/wd/hub')
       .withCapabilities({
         'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
         build: process.env.TRAVIS_BUILD_NUMBER,
-        username: process.env."AmmarahCassim",
+        username: process.env."AmmarahCassim,
         accessKey: process.env."24302255-f2d7-46f5-ac1c-9c90ec0eff11",
         browserName: "chrome"
       }).build();
@@ -16,5 +20,21 @@
       }).build();
     }
 
-    return this.browser.get("http://localhost:3000/views/index.ejs");
+    return this.browser.get("http://localhost:3000/");
+  });
+
+  afterEach(function() {
+    return this.browser.quit();
+  });
+
+  it("should handle clicking on a headline", function(done) {
+    var headline = this.browser.findElement(webdriver.By.css('h1'));
+
+    headline.click();
+
+    headline.getText().then(function(txt) {
+      assert.equal(txt, "awesome");
+      done();
+    });
+  });
 });
