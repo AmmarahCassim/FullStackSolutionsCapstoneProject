09bc574eadabd212c955b91518d2940d32c13c75
Tcoco4 <36608817+Tcoco4@users.noreply.github.com>
2018-07-23 21:03:39 +0200

    Update

---
+++ javascript/script.js
@@ -0,0 +1,36 @@
+    $(document).ready(function(){
+    $("#generate").click(function(e){  
+      console.log("generating");
+      $.get("/text", function(data, status){
+       // alert('found SCRIPT');
+        console.log("I AM WORKING");
+        $("textarea").append(data); 
+
+      })
+      .done(function(){
+        
+        $("textarea").append(data); 
+        alert("callback bitches");
+      })
+    });
+
+
+    
+
+    $("#EXPORT").click(function(){
+      $.get("/pidgin_breakdown", function(data, status){
+          console.log("Data: " + data + "\nStatus: " + status);
+      });
+    });
+
+    $("#EDIT").click(function(){
+      console.log("heita");
+      var text = $("#speechtotext").val();
+      console.log(text);
+      $.post("/update",{output: text},function(data,status){
+        console.log("Data: " + data);
+      });  
+    });
+
+    
+});
